8c6dd99530a3991c6a13d536d39fd6ceb0a2d2b7
PHUPHU2310 <thieukhanhphu2310@gmail.com>
2025-09-09 12:09:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const greetingElem = document.querySelector('.greeting');
+const now = new Date();
+const hour = now.getHours();
+// Hiển thị phần giới thiệu bản thân
+
+
+greetingElem.textContent = " Có gì thì kết nối với mình qua 4 cái ai cừn mình đã gắn nó ở dưới nhé , hihi =>>";
